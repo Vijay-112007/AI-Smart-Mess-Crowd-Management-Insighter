c0d15dfd3cbcee084005a218dbc8ff7decaea665
K.Vijaya Sai <kattamudivijaysai@gmail.com>
2026-01-03 12:35:14 +0530

    Added functions inside the main function
    Done with the prototype basic and interface is ready

--- main.py
+++ main.py
@@ -1,19 +1,16 @@
-from authentication import Authentication,SignIn,SignUp
-import math
-import numpy as np
-import cv2 as cv
+from authentication import SignIn,SignUp
 from crowd import VideoAnalytics
 import time
 def main():
     #first we will ask either to sign up or sign in
-    choice = int(input("1. Sign Up \n 2. Login In \n Enter Your Choice: "))
+    choice = int(input("1. Sign Up\n2. Login In\nEnter Your Choice: "))
     if choice == 1:
         choice1 = int(input("1.Sign Up as Admin\n2.Sign Up as User\nEnter Your Choice: "))
-        if choice1 == 1:
-            newadmin = SignUp(input("Enter Your New Username:"),input("Enter Your New Password: "),input("Enter Your Email:"))
+        if choice1 == 2:
+            newuser = SignUp(input("Enter Your New Username:"),input("Enter Your New Password: "),input("Enter Your Email:"),0)
             print("Signing In......")
             time.sleep(10)
-            print(newadmin.sign_in())
+            print(newuser.sign_in())
             #we need to provide three options after login into the system
             print("1. Crowd Analytics\n2.Scan the QR\n3.Food Insights")
             choice2 = input("Enter Your Choice: ")
@@ -21,8 +18,38 @@ def main():
                 case "1":
                     va1 = VideoAnalytics("C:/Users/vijay/OneDrive/Concepts/Coding Concepts/14706612_1920_1080_60fps.mp4","C:/Users/vijay/OneDrive/Concepts/Coding Concepts/YoloWeights/yolov8n.pt")
                     print("Press \"D\" to Stop the Video")
+                    time.sleep(10)
                     va1.videoanalysis()
                     print("Video Successfully Ended")
+                case "2":
+                    print("Coming Soon....")
+                    time.sleep(10)
+                case "3":
+                    print("Coming Soon....")
+                    time.sleep(10)
+        elif choice1 == 1:
+            newadmin = SignUp(input("Enter Your New Username:"),input("Enter Your New Password: "),input("Enter Your Email:"))
+            print("Signing In......")
+            time.sleep(10)
+            print(newadmin.sign_in())
+            #we need to provide three options after login into the system
+            print("1. Crowd Analytics\n2.Start Generating QR code\n3.Food Insights\n4. Monthly Insights")
+            choice2 = input("Enter Your Choice: ")
+            match(choice2):
+                case "1":
+                    va1 = VideoAnalytics("C:/Users/vijay/OneDrive/Concepts/Coding Concepts/14706612_1920_1080_60fps.mp4","C:/Users/vijay/OneDrive/Concepts/Coding Concepts/YoloWeights/yolov8n.pt")
+                    print("Press \"D\" to Stop the Video")
+                    print("Loading the Analytics....")
+                    time.sleep(10)
+                    va1.videoanalysis()
+                    time.sleep(5)
+                    print("Video Successfully Ended")
+                case "2":
+                    print("Coming Soon....")
+                    time.sleep(10)
+                case "3":
+                    print("Coming Soon....")
+                    time.sleep(10)
     elif choice == 2:
         choice1 = int(input("1.Sign In as Admin\n2.Sign In as User\nEnter Your Choice: "))
         if choice1 == 1:
@@ -30,15 +57,54 @@ def main():
             print("Signing In......")
             time.sleep(10)
             print(newadmin.sign_in())
+            newadmin.authenticated = True
             #we need to provide three options after login into the system
-            print("1. Crowd Analytics\n2.Scan the QR\n3.Food Insights")
+            print("1. Crowd Analytics\n2.Start Generating QR code\n3.Food Insights\n4. Monthly Insights\n5.Exit")
             choice2 = input("Enter Your Choice: ")
             match(choice2):
                 case "1":
                     va1 = VideoAnalytics("C:/Users/vijay/OneDrive/Concepts/Coding Concepts/14706612_1920_1080_60fps.mp4","C:/Users/vijay/OneDrive/Concepts/Coding Concepts/YoloWeights/yolov8n.pt")
                     print("Press \"D\" to Stop the Video")
+                    print("Loading the Analytics....")
+                    time.sleep(10)
+                    va1.videoanalysis()
+                    time.sleep(5)
+                    print("Video Successfully Ended")
+                case "2":
+                    print("Coming Soon....")
+                    time.sleep(10)
+                case "3":
+                    print("Coming Soon....")
+                    time.sleep(10)
+                case "4":
+                    print("Coming Soon....")
+                    time.sleep(10)
+                case "5":
+                    newadmin.authenticated = False
+        elif choice1 == 2:
+            newuser = SignIn(input("Enter Your Username:"),input("Enter Your Password: "),0)
+            print("Signing In......")
+            time.sleep(10)
+            print(newuser.sign_in())
+            #we need to provide three options after login into the system
+            print("1. Crowd Analytics\n2.Scan the QR\n3.Food Insights\n4.Exit")
+            newuser.authenticated = True
+            choice2 = input("Enter Your Choice: ")
+            match(choice2):
+                case "1":
+                    va1 = VideoAnalytics("C:/Users/vijay/OneDrive/Concepts/Coding Concepts/14706612_1920_1080_60fps.mp4","C:/Users/vijay/OneDrive/Concepts/Coding Concepts/YoloWeights/yolov8n.pt")
+                    print("Press \"D\" to Stop the Video")
+                    time.sleep(10)
                     va1.videoanalysis()
                     print("Video Successfully Ended")
+                case "2":
+                    print("Coming Soon....")
+                    time.sleep(10)
+                case "3":
+                    print("Coming Soon....")
+                    time.sleep(10)
+                case "4":
+                    newuser.authenticated = False
     pass
 if __name__ == "__main__":
     main()
